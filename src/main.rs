e64aafdb967bffc8e36118bfef22c94003243823
trip-flip <thomasbaugher@outlook.com>
2022-07-01 11:05:20 -0400

    Allow data elements to wrap

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::env::args;
 
 mod bf {
+    use std::num::Wrapping;
+
     pub fn parse(input: &[u8]) {
         let mut data_index = 0;
         let mut input_index = 0;
-        let mut data: Vec<u8> = vec![0];
+        let mut data: Vec<Wrapping<u8>> = vec![Wrapping(0)];
         let mut loop_i = Vec::new();
         let mut skip = false;
         let mut last_len = 0;
@@ -17,13 +19,13 @@ mod bf {
                     '>' => {
                         data_index += 1;
                         if data_index >= data.len() {
-                            data.resize(data_index + 1, 0);
+                            data.resize(data_index + 1, Wrapping(0));
                         }
                     },
                     '<' => data_index -= 1,
                     '+' => data[data_index] += 1,
                     '-' => data[data_index] -= 1,
-                    '.' => print!("{}", data[data_index] as char),
+                    '.' => print!("{}", data[data_index].0 as char),
                     ',' => (),
                     _ => ()
                 }
@@ -32,7 +34,7 @@ mod bf {
             match c as char {
                 '[' => {
                     loop_i.push(input_index);
-                    if data[data_index] != 0 { // Predicate true
+                    if data[data_index].0 != 0 { // Predicate true
                         last_len = loop_i.len();
                     } else {
                         skip = true;
@@ -45,7 +47,7 @@ mod bf {
                             skip = false;
                         }
                     } else {
-                        if data[data_index] == 0 {
+                        if data[data_index].0 == 0 {
                             loop_i.pop();
                         } else {
                             input_index = *loop_i.last().unwrap();
